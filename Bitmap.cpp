acdc39c6876f7f37473ec14b33e7a0c871dd5416
empty-works <pama.mark@gmail.com>
2020-06-23 08:42:53 +0200

    Added write function to Bitmap to write to a file.

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -4,6 +4,10 @@ Bitmap::Bitmap(int width, int height): m_width(width), m_height(height) {
 
 }	
 
+bool Bitmap::write(std::string filename) {
+	
+}
+
 Bitmap::~Bitmap() {
 
 }
--- Bitmap.h
+++ Bitmap.h
@@ -1,12 +1,15 @@
 #ifndef BITMAP_H_
 #define BITMAP_H_
 
+#include <string>
+
 class Bitmap {
 	int m_width{0};
 	int m_height{0};
 
 	public:
 	Bitmap(int width, int height);
+	bool write(std::string filename);
 	virtual ~Bitmap();
 };
 
