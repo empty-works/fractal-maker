2856546deb6584c41648982935e1743f7a2a9f53
empty-works <pama.mark@gmail.com>
2020-06-24 23:04:57 +0200

    Set bitmap file header file size and offset.

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -7,10 +7,16 @@ Bitmap::Bitmap(int width, int height): m_width(width), m_height(height), m_pPixe
 }	
 
 void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
-
+	
 }
 
 bool Bitmap::write(std::string filename) {
+	BitmapFileHeader fileHeader;
+	BitmapInfoHeader infoHeader;
+	fileHeader.fileSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + m_width * m_height * 3;
+	fileHeader.dataOffset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+
+
 	return false;	
 }
 
