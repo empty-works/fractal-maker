ac6c18b694031c95cc45dfcd6aa84109885d9d60
empty-works <pama.mark@gmail.com>
2020-06-26 07:26:32 +0200

    Added scaling in the main driver.

--- FractalMaker.cpp
+++ FractalMaker.cpp
@@ -7,6 +7,20 @@ int main() {
 
 	Bitmap bitmap(WIDTH, HEIGHT);
 
+	double min {999999};
+	double max {-999999};
+
+	for(int y {0}; y < HEIGHT; y++) {
+		for(int x {0}; x < WIDTH; x++) {
+			double xFractal {0};
+			double yFractal {0};
+
+			if(xFractal < min) min = xFractal;		
+			if(xFractal < max) max = xFractal;		
+		}
+	}
+	
+	std::cout << min << ", " << max << std::endl;
 
 	bitmap.write("test.bmp");
 
