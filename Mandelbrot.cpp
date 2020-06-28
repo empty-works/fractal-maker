fe850043936233481be5eb240b208c8a65ed77b2
empty-works <pama.mark@gmail.com>
2020-06-28 23:16:34 +0200

    Created .cpp file for the Mandelbrot header.

---
+++ Mandelbrot.cpp
@@ -0,0 +1,13 @@
+#include "Mandelbrot.h"
+
+Mandelbrot::Mandelbrot() {
+
+}
+
+Mandelbrot::~Mandelbrot() {
+
+}
+
+int Mandelbrot::getIterations(double x, double y) {
+
+}
--- Mandelbrot.h
+++ Mandelbrot.h
@@ -5,6 +5,8 @@ class Mandelbrot {
 public:
 	Mandelbrot();
 	virtual ~Mandelbrot();
+
+	static int getIterations(double x, double y);
 };
 
 #endif
