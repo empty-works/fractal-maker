b2ff855be0281fadfe3330ac6c122d34a12a0f5a
empty-works <pama.mark@gmail.com>
2020-06-29 02:28:20 +0200

    Adjusted colors to the fractal to make them pop more.

--- FractalMaker.cpp
+++ FractalMaker.cpp
@@ -14,16 +14,17 @@ int main() {
 
 	for(int y {0}; y < HEIGHT; y++) {
 		for(int x {0}; x < WIDTH; x++) {
-			double xFractal {(x - WIDTH / 2 - 200) * 2.0 / WIDTH};
-			double yFractal {(y - WIDTH / 2) * 2.0 / WIDTH};
+			double xFractal {(x - WIDTH / 2 - 200) * 2.0 / HEIGHT};
+			double yFractal {(y - HEIGHT / 2) * 2.0 / HEIGHT};
 
 			int iterations = Mandelbrot::getIterations(xFractal, yFractal);
-			uint8_t red = (uint8_t)(256 * (double)iterations/Mandelbrot::MAX_ITERATIONS);
+			uint8_t color = (uint8_t)(256 * (double)iterations/Mandelbrot::MAX_ITERATIONS);
 
-			bitmap.setPixel(x, y, red, red, red);
+			color = color * color * color;
+			bitmap.setPixel(x, y, 0, color, 0);
 
-			if(red < min) min = red;		
-			if(red > max) max = red;		
+			if(color < min) min = color;		
+			if(color > max) max = color;		
 		}
 	}
 	
