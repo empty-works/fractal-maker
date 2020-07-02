beaad6d39e89deb1a51daef9db240f23aee4eac2
empty-works <pama.mark@gmail.com>
2020-07-02 22:21:55 +0200

    Using histogram.

--- FractalMaker.cpp
+++ FractalMaker.cpp
@@ -40,18 +40,17 @@ int main() {
 	for(size_t y {0}; y < HEIGHT; y++) {
 		for(size_t x {0}; x < WIDTH; x++) {
 			int iterations = fractal[y * WIDTH + x];
-			uint8_t color = (uint8_t)(256 * (double)iterations/Mandelbrot::MAX_ITERATIONS);
 
 			double hue {0.0};
 			for(size_t i {0}; i <= iterations; i++) {
 				hue += ((double)histogram[i]) / total;
 			}
 
-			color = color * color * color;
-			bitmap.setPixel(x, y, 0, color, 0);
+			uint8_t red = 0;
+			uint8_t green = hue * 255;
+			uint8_t blue = 0;
 
-			if(color < min) min = color;		
-			if(color > max) max = color;		
+			bitmap.setPixel(x, y, red, green, blue);
 		}
 	}
 
