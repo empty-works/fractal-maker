27bc05f09b2b495e3a49a76aa4a90d16ca1a916f
empty-works <pama.mark@gmail.com>
2020-07-02 22:40:42 +0200

    Initialized members of Zoom.

--- Zoom.h
+++ Zoom.h
@@ -1,9 +1,13 @@
 #ifndef ZOOM_H_
 #define ZOOM_H_
 
-class Zoom {
-public:
-	Zoom();
+struct Zoom {
+	// Coordinates we want to zoom to.
+	int x {0};
+	int y {0};
+	double scale {0.0};
+
+	Zoom(int x, int y, double scale): x(x), y(y), scale(scale) {}
 };
 
 
