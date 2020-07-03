49334675b438a139ed0511f5b4e29a1c25638f1f
empty-works <pama.mark@gmail.com>
2020-07-03 23:40:30 +0200

    Added new variables to ZoomList and initialized.

--- ZoomList.cpp
+++ ZoomList.cpp
@@ -1,7 +1,7 @@
 #include "ZoomList.h"
 
 ZoomList::ZoomList(int width, int height): m_width(width), m_height(height) {
-
+	
 }
 
 void ZoomList::add(const Zoom &zoom) {
--- ZoomList.h
+++ ZoomList.h
@@ -7,6 +7,10 @@
 
 class ZoomList {
 private:
+	double m_xCenter{0};
+	double m_yCenter{0};
+	double m_scale{0};
+
 	int m_width {0};
 	int m_height {0};
 	std::vector<Zoom> zooms;
