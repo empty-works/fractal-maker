694473cfb788ca01ba856ebdf9858a6bca4e5e63
empty-works <pama.mark@gmail.com>
2020-07-04 06:46:52 +0200

    Refactored main class and added methods to ZoomList to accommodate the code from main.

--- FractalCreator.cpp
+++ FractalCreator.cpp
@@ -1,7 +1,12 @@
 #include "FractalCreator.h"
 
-FractalCreator::FractalCreator(int width, int height) {
+FractalCreator::FractalCreator(int width, int height): m_width(width), m_height(height),
+ m_histogram(new int[Mandelbrot::MAX_ITERATIONS] {0}), 
+ m_fractal(new int[m_width * m_height]{0}), 
+ m_bitmap(m_width, m_height),
+ m_zoomList(m_width, m_height) {
 		
+	m_zoomList.add(Zoom(m_width / 2, m_height / 2, 4.0 / m_width));
 }
 
 FractalCreator::~FractalCreator() {
@@ -10,16 +15,56 @@ FractalCreator::~FractalCreator() {
 
 void FractalCreator::calculateIteration() {
 
+	for(int y {0}; y < m_height; y++) {
+		for(int x {0}; x < m_width; x++) {
+			std::pair<double, double> coords = m_zoomList.doZoom(x, y);
+			int iterations = Mandelbrot::getIterations(coords.first, coords.second);
+
+			m_fractal[y * m_width + x] = iterations;
+
+			if(iterations != Mandelbrot::MAX_ITERATIONS) {
+				m_histogram[iterations]++;
+			}	
+		}
+	}
+}
+
+void FractalCreator::calculateTotalIterations() {
+
+	for(size_t i {0}; i < Mandelbrot::MAX_ITERATIONS; i++) {
+		m_total += m_histogram[i];
+	}
 }
 
 void FractalCreator::drawFractal() {
 
+	for(size_t y {0}; y < m_height; y++) {
+		for(size_t x {0}; x < m_width; x++) {
+			uint8_t red = 0;
+			uint8_t green = 0; 
+			uint8_t blue = 0;
+
+			int iterations = m_fractal[y * m_width + x];
+
+			if(iterations != Mandelbrot::MAX_ITERATIONS) {
+			
+				double hue {0.0};
+				for(size_t i {0}; i <= iterations; i++) {
+					hue += ((double)m_histogram[i]) / m_total;
+				}
+
+				green = std::pow(255, hue); 
+			}
+
+			m_bitmap.setPixel(x, y, red, green, blue);
+		}
+	}
 }
 
 void FractalCreator::addZoom(const Zoom & zoom) {
-
+	m_zoomList.add(zoom);
 }
 
 void FractalCreator::writeBitmap(std::string name) {
-
+	m_bitmap.write(name);
 }
--- FractalCreator.h
+++ FractalCreator.h
@@ -1,19 +1,31 @@
 #ifndef FRACTALCREATOR_H_
 #define FRACTALCREATOR_H_
 
+#include <iostream>
+#include <cstdint>
+#include <math.h>
+#include <memory>
 #include <string>
 #include "Zoom.h"
+#include "Mandelbrot.h"
+#include "Bitmap.h"
+#include "ZoomList.h"
 
 class FractalCreator {
 private:
-
+	int m_width;
+	int m_height;
 	std::unique_ptr<int[]> m_histogram;
-	std::unique_ptr<int[]> m_fractal(new int[m_width * m_height]{0});
+	std::unique_ptr<int[]> m_fractal;
+	Bitmap m_bitmap;
+	ZoomList m_zoomList;
+        int m_total {0};
 public:
 	FractalCreator(int width, int height);
 	virtual ~FractalCreator();
 
 	void calculateIteration();
+	void calculateTotalIterations();
 	void drawFractal();
 	void addZoom(const Zoom & zoom);
 	void writeBitmap(std::string name);
